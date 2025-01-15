f1508647e385da28c7423e0ad192426b1ad8f9f8
carloscoqqueiro <carloseducoqueiro90@gmail.com>
2025-01-14 23:04:37 -0300

    Desafio do Felipão DIO

---
+++ index.py
@@ -0,0 +1,22 @@
+nome_heroi = "Carlos"
+xp = 5580
+
+while True:
+  if (xp < 1000):
+      print("ferro")
+  if (xp >= 1001 and xp <= 2000):
+    print("bronze")
+  if (xp >= 2001 and xp <= 5000):
+    print("prata")
+  if (xp >= 5001 and xp <= 7000):
+    print("ouro")
+  if (xp >= 7001 and xp <= 8000):
+    print("platina")
+  if (xp >= 8001 and xp <= 9000):
+    print("ascendente")
+  if (xp >= 9001 and xp <= 10000):
+    print("imortal")
+  if (xp >= 10001):
+    print("radiante")
+
+  break
